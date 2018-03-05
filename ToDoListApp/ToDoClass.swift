81f8bf6ee3c9988680184471ad3e796c932cc7e8
Brian Estep <bestep@gmail.com>
2018-03-05 17:24:56 -0500

    Added ToDo Class

---
+++ ToDoListApp/ToDoClass.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoClass.swift
+//  ToDoListApp
+//
+//  Created by Brian Estep on 3/5/18.
+//  Copyright © 2018 Brian Estep. All rights reserved.
+//
+
+import UIKit
+
+class ToDoItem {
+    var ToDoName : String = ""
+    var ToDoImportant : Bool = false
+}
